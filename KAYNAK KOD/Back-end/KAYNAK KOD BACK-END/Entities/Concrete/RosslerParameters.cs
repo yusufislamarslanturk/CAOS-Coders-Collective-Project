3d4eef1dbc8f96383d9ae7fdfc79ba3b9e6edbb9
Yusuf İslam Arslantürk <205541050@firat.edu.tr>
2024-04-08 22:34:13 +0300

    Rossler metodu eklendi.

---
+++ KAYNAK KOD/Back-end/KAYNAK KOD BACK-END/Entities/Concrete/RosslerParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class RosslerParameters
+    {
+        public double[] InitialState { get; set; }
+        public double Dt { get; set; }
+        public double σ { get; set; }
+        public double β { get; set; }
+        public double ρ { get; set; }
+    }
+}
---
+++ KAYNAK KOD/Back-end/KAYNAK KOD BACK-END/WebAPI/Controllers/RosslerController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RosslerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult RosslerMethod([FromBody] RosslerParameters parameters)
+        {
+            double[] s = parameters.InitialState;
+            double dt = parameters.Dt;
+            double σ = parameters.σ;
+            double β = parameters.β;
+            double ρ = parameters.ρ;
+
+            RosslerManager.RosslerMethod(s, dt, σ, β, ρ);
+
+            return Ok(s);
+        }
+    }
+}
